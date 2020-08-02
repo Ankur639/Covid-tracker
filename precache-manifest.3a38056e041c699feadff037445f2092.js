15c5d4355bd397a224ad0e9a5c1ff981b1479129
Kumar,Ankur <Ankur.Kumar@cerner.com>
2020-08-02 20:00:56 +0530

    Updates

---
+++ precache-manifest.3a38056e041c699feadff037445f2092.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8068d0ddb97a6535ada542b893c2adab",
+    "url": "/Covid-tracker/index.html"
+  },
+  {
+    "revision": "0773ac85bb9bb7e94c5d",
+    "url": "/Covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "f5e00088704b88bdd5b2",
+    "url": "/Covid-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "0773ac85bb9bb7e94c5d",
+    "url": "/Covid-tracker/static/js/2.c65fada2.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-tracker/static/js/2.c65fada2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5e00088704b88bdd5b2",
+    "url": "/Covid-tracker/static/js/main.7fd90407.chunk.js"
+  },
+  {
+    "revision": "963e8b5cd94a8831c64e",
+    "url": "/Covid-tracker/static/js/runtime-main.9c09fe4f.js"
+  }
+]);
